5cf9831fddeb90e5ce79ccfecc9c351bca9392ea
sujeongHeo401 <gotnwjd40@gmail.com>
2022-05-19 01:12:21 +0900

    전진과 후진 ~~

--- car.js
+++ car.js
@@ -9,7 +9,7 @@ class Car{
         this.acceleration=0.2;
         this.maxSpeed=3;
         this.friction=0.05;
-
+        this.angle = 0;
         this.controls= new Controls();
     }
 
@@ -30,22 +30,43 @@ class Car{
         }
 
         if(this.speed > 0 ){
-            this.spped -= this.friction;
+            this.speed -= this.friction;
         }
         if(this.speed < 0 ){
-            this.spped += this.friction;
+            this.speed += this.friction;
         }
-        this.y -= this.speed;
+
+        if(Math.abs(this.speed) < this.friction){
+            this.speed = 0;
+        }
+
+        if(this.controls.left){
+            this.angle+=0.03;
+        }
+        if(this.controls.right){
+            this.angle -= 0.03;
+
+        }
+        this.x -= Math.sin(this.angle)*this.speed;
+        this.y -= Math.cos(this.angle)*this.speed;
+    
+        // 선분의 길이가 1cm 인 경우 사인은 각도에 대한 상승을 나타내며 동일한 길이의 선에 대해 Cos는 각도에 대한 런을 나타냅니다.
     }
 
     draw(ctx){
+
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width/2,
-            this.y - this.height/2,
+            - this.width/2,
+            - this.height/2,
             this.width,
             this.height
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }
